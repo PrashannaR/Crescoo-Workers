e12220934f08d5fbfc87eeadaa3a19eafd8d355f
PrashannaR <prashannna.rajbhandari1@gmail.com>
2022-03-02 21:59:55 +0545

    Login and Signup Packages Setup

---
+++ app/src/main/java/com/example/crescooworkers/Login/LoginOTP.java
@@ -0,0 +1,16 @@
+package com.example.crescooworkers.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.crescooworkers.R;
+
+public class LoginOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_otp);
+    }
+}
